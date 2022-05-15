52cf9d0766f90b2367e6cba069bdc6e4715db477
raphroncal <raph.roncal01@gmail.com>
2022-05-16 01:36:31 +0800

    cleaning

--- algorithm.h
+++ algorithm.h
@@ -1,8 +1,12 @@
 extern const int COL;
 
-void printArray(int, int[][COL]);
+void firstComeFirstServe(int, int[][COL]);
 
-void output(int, int[][COL]);
+void shortestJobFirst(int, int[][COL]);
+
+void shortestRemainingTimeFirst(int, int[][COL]);
+
+void roundRobin(int, int, int[][COL]);
 
 void sortByBurstTime(int, int[][COL]);
 
@@ -12,10 +16,6 @@ int isDone(int, int[][COL]);
 
 int hasAnyArrived(int, int[][COL]);
 
-void firstComeFirstServe(int, int[][COL]);
+void printArray(int, int[][COL]);
 
-void shortestJobFirst(int, int[][COL]);
-
-void shortestRemainingTimeFirst(int, int[][COL]);
-
-void roundRobin(int, int, int[][COL]);
+void output(int, int[][COL]);
